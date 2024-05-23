efcf9ff5326cf4e5ed65270e02c4782360b99fe6
celeste0423 <jyeob25@gmail.com>
2024-05-23 13:46:54 +0900

    api추가

--- lib/features/station_page/station_page.dart
+++ lib/features/station_page/station_page.dart
@@ -1,12 +1,69 @@
+import 'dart:convert';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:subway_seat/helpers/open_alert_dialog.dart';
 import 'package:subway_seat/utils/custom_color.dart';
 import 'package:subway_seat/widgets/title_text.dart';
 
-class StationPage extends StatelessWidget {
-  StationPage({super.key});
+class StationPage extends StatefulWidget {
+  final String? stationCode;
+  final String? isUpward;
+  StationPage({super.key, this.stationCode, this.isUpward});
 
+  @override
+  State<StationPage> createState() => _StationPageState();
+}
+
+class _StationPageState extends State<StationPage> {
+  @override
+  void initState() {
+    searchTime(widget.stationCode!, widget.isUpward!);
+    super.initState();
+  }
+
+  bool isLoading = true;
+
+  List<List<String>> trainInfoList = [];
   String subwayApiKey = '4a687773456a79653737634c46526b';
+  String serviceUrl = '//openAPI.seoul.go.kr:8088';
+
+  Future<List<List<String>>> searchTime(
+      String stationCode, String isUpward) async {
+    print('코드는 ${stationCode}');
+    // String apiUrl =
+    //     '${serviceUrl}/${subwayApiKey}/json/SearchSTNTimeTableByIDService/1/5/2561/1/1/';
+    String apiUrl =
+        '${serviceUrl}/${subwayApiKey}/json/SearchSTNTimeTableByIDService/1/120/${stationCode}/1/${isUpward}';
+    print(apiUrl);
+    http.Response response = await http.get(Uri.parse(apiUrl));
+    if (response.statusCode == 200) {
+      // print(response.body);
+      List<List<String>> trainInfos = [];
+      final Map<String, dynamic> data = json.decode(response.body);
+      final List<dynamic> rows = data['SearchSTNTimeTableByIDService']['row'];
+
+      for (var row in rows) {
+        String arrivalTime = row['ARRIVETIME'];
+        String trainNo = row['TRAIN_NO'];
+        trainInfos.add([arrivalTime, trainNo]);
+      }
+      print('열차 시간 정보 ${trainInfos}');
+      trainInfoList = trainInfos;
+      setState(() {
+        isLoading = false; // 로딩 완료
+      });
+      return trainInfos;
+    } else {
+      setState(() {
+        isLoading = false; // 로딩 완료
+      });
+      throw openAlertDialog(
+          title: '열차 정보 로드에 실패했습니다.',
+          content: '에러코드: ${response.statusCode.toString()}');
+    }
+  }
 
   PreferredSizeWidget _appBar() {
     return AppBar(
@@ -16,23 +73,23 @@ class StationPage extends StatelessWidget {
     );
   }
 
-  Widget _stationListView() {
+  Widget _timeListView() {
     return ListView.builder(
-      itemCount: 51,
+      itemCount: trainInfoList.length,
       itemBuilder: (context, index) {
-        return _stationCard(index);
+        return _timeCard(index);
       },
     );
   }
 
-  Widget _stationCard(int index) {
+  Widget _timeCard(int index) {
     return CupertinoButton(
       onPressed: () {},
       padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
       alignment: Alignment.centerLeft,
       child: Container(
         child: Text(
-          stationList[index][0],
+          '도착시간 : ${trainInfoList[index][0]}, 열차코드 : ${trainInfoList[index][1]}',
           style: TextStyle(
             fontSize: 20,
             color: CustomColors.blackText,
@@ -46,7 +103,9 @@ class StationPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: _appBar(),
-      body: _stationListView(),
+      body: isLoading
+          ? Center(child: CircularProgressIndicator()) // 로딩 중이면 로딩 표시
+          : _timeListView(),
     );
   }
 }
--- lib/features/station_select/station_select_page.dart
+++ lib/features/station_select/station_select_page.dart
@@ -1,63 +1,71 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:subway_seat/features/station_page/station_page.dart';
 import 'package:subway_seat/utils/custom_color.dart';
 import 'package:subway_seat/widgets/title_text.dart';
 
-class StationSelectPage extends StatelessWidget {
+class StationSelectPage extends StatefulWidget {
   StationSelectPage({super.key});
 
+  @override
+  State<StationSelectPage> createState() => _StationSelectPageState();
+}
+
+class _StationSelectPageState extends State<StationSelectPage> {
+  String isUpward = '1';
   List<List<dynamic>> stationList = [
-    ['진접(경복대)', 405],
-    ['오남', 406],
-    ['별내별가람', 408],
-    ['당고개', 409],
-    ['상계', 410],
-    ['노원', 411],
-    ['창동', 412],
-    ['쌍문', 413],
-    ['수유(강북구청)', 414],
-    ['미아(서울사이버대학)', 415],
-    ['미아사거리', 416],
-    ['길음', 417],
-    ['성신여대입구(돈암)', 418],
-    ['한성대입구(삼선교)', 419],
-    ['혜화', 420],
-    ['동대문', 421],
-    ['동대문역사문화공원(DDP)', 422],
-    ['충무로', 423],
-    ['명동(우리금융타운)', 424],
-    ['회현(남대문시장)', 425],
-    ['서울역', 426],
-    ['숙대입구(갈월)', 427],
-    ['삼각지', 428],
-    ['신용산(아모레퍼시픽)', 429],
-    ['이촌(국립중앙박물관)', 430],
-    ['동작(현추원)', 431],
-    ['총신대입구(이수)', 432],
-    ['사당', 433],
-    ['남태령', 434],
-    ['선바위', 435],
-    ['경마공원', 436],
-    ['대공원(서울랜드)', 437],
-    ['과천', 438],
-    ['정부과천청사', 439],
-    ['인덕원', 440],
-    ['평촌(한림대성심병원)', 441],
-    ['범계', 442],
-    ['금정', 443],
-    ['산본(원광대산본병원)', 444],
-    ['수리산', 445],
-    ['대야미', 446],
-    ['반월', 447],
-    ['상록수(안산대학교)', 448],
-    ['한대앞', 449],
-    ['중앙(서울예술대학교)', 450],
-    ['고잔(고대안산병원)', 451],
-    ['초지(신안산대학교)', 452],
-    ['안산', 453],
-    ['신길온천', 454],
-    ['정왕(한국공학대학교)', 455],
-    ['오이도', 456],
+    ['진접(경복대)', '0405'],
+    ['오남', '0406'],
+    ['별내별가람', '0408'],
+    ['당고개', '0409'],
+    ['상계', '0410'],
+    ['노원', '0411'],
+    ['창동', '0412'],
+    ['쌍문', '0413'],
+    ['수유(강북구청)', '0414'],
+    ['미아(서울사이버대학)', '0415'],
+    ['미아사거리', '0416'],
+    ['길음', '0417'],
+    ['성신여대입구(돈암)', '0418'],
+    ['한성대입구(삼선교)', '0419'],
+    ['혜화', '0420'],
+    ['동대문', '0421'],
+    ['동대문역사문화공원(DDP)', '0422'],
+    ['충무로', '0423'],
+    ['명동(우리금융타운)', '0424'],
+    ['회현(남대문시장)', '0425'],
+    ['서울역', '0426'],
+    ['숙대입구(갈월)', '0427'],
+    ['삼각지', '0428'],
+    ['신용산(아모레퍼시픽)', '0429'],
+    ['이촌(국립중앙박물관)', '0430'],
+    ['동작(현추원)', '0431'],
+    ['총신대입구(이수)', '0432'],
+    ['사당', '0433'],
+    ['남태령', '0434'],
+    ['선바위', '0435'],
+    ['경마공원', '0436'],
+    ['대공원(서울랜드)', '0437'],
+    ['과천', '0438'],
+    ['정부과천청사', '0439'],
+    ['인덕원', '0440'],
+    ['평촌(한림대성심병원)', '0441'],
+    ['범계', '0442'],
+    ['금정', '0443'],
+    ['산본(원광대산본병원)', '0444'],
+    ['수리산', '0445'],
+    ['대야미', '0446'],
+    ['반월', '0447'],
+    ['상록수(안산대학교)', '0448'],
+    ['한대앞', '0449'],
+    ['중앙(서울예술대학교)', '0450'],
+    ['고잔(고대안산병원)', '0451'],
+    ['초지(신안산대학교)', '0452'],
+    ['안산', '0453'],
+    ['신길온천', '0454'],
+    ['정왕(한국공학대학교)', '0455'],
+    ['오이도', '0456'],
   ];
 
   PreferredSizeWidget _appBar() {
@@ -65,6 +73,74 @@ class StationSelectPage extends StatelessWidget {
       title: TitleText(
         text: '역 선택',
       ),
+    );
+  }
+
+  Widget _isUpwardSelect() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        CupertinoButton(
+          padding: const EdgeInsets.symmetric(horizontal: 5),
+          onPressed: () {
+            setState(() {
+              isUpward = '1';
+            });
+          },
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.transparent,
+              borderRadius: BorderRadius.circular(15),
+              border: Border.all(
+                color: isUpward == '1'
+                    ? CustomColors.mainBlue
+                    : CustomColors.greyBackground,
+                width: 1,
+              ),
+            ),
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+            child: Text(
+              '상행선',
+              style: TextStyle(
+                color: isUpward == '1'
+                    ? CustomColors.mainBlue
+                    : CustomColors.greyBackground,
+                fontSize: 12,
+              ),
+            ),
+          ),
+        ),
+        CupertinoButton(
+          padding: const EdgeInsets.symmetric(horizontal: 5),
+          onPressed: () {
+            setState(() {
+              isUpward = '2';
+            });
+          },
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.transparent,
+              borderRadius: BorderRadius.circular(15),
+              border: Border.all(
+                color: isUpward == '2'
+                    ? CustomColors.mainBlue
+                    : CustomColors.greyBackground,
+                width: 1,
+              ),
+            ),
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+            child: Text(
+              '하행선',
+              style: TextStyle(
+                color: isUpward == '2'
+                    ? CustomColors.mainBlue
+                    : CustomColors.greyBackground,
+                fontSize: 12,
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 
@@ -79,8 +155,13 @@ class StationSelectPage extends StatelessWidget {
 
   Widget _stationCard(int index) {
     return CupertinoButton(
-      onPressed: () {},
-      padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+      onPressed: () {
+        Get.to(() => StationPage(
+              stationCode: stationList[index][1].toString(),
+              isUpward: isUpward,
+            ));
+      },
+      padding: EdgeInsets.symmetric(vertical: 10),
       alignment: Alignment.centerLeft,
       child: Container(
         child: Text(
@@ -98,7 +179,15 @@ class StationSelectPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: _appBar(),
-      body: _stationListView(),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30),
+        child: Column(
+          children: [
+            _isUpwardSelect(),
+            Expanded(child: _stationListView()),
+          ],
+        ),
+      ),
     );
   }
 }
